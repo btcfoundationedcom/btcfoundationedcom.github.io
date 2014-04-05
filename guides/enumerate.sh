780e68474a69956d628404b261a691420ec4aadf
Nikos Bentenitis <nikos.bentenitis@gmail.com>
2014-04-05 15:40:31 -0500

    Update

--- guides/enumerate.sh
+++ guides/enumerate.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 for i in *.md
 do
+	REPO="https://github.com/btcfoundationedcom/btcfoundationedcom.github.io/raw/master/guides/"
 	FNAME=`basename $i .md`
 	TITLE=`head -1 ${FNAME}.md | sed "s/\% //"`
-	LETTER=`echo ${FNAME}_letter.pdf`
-	A4=`echo ${FNAME}_a4.pdf`
-	HTML=`echo ${FNAME}.html`
+	LETTER="$REPO/${FNAME}_letter.pdf"
+	A4="$REPO/${FNAME}_a4.pdf"
+	HTML="$REPO/${FNAME}.html"
 	echo "- ${TITLE}: [PDF letter size]($LETTER), [PDF A4 size]($A4), [html]($HTML)"
 done
